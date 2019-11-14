9921df95c4c5f44426e0976ca5f129215d5cadbf
Mateus Oliveira <mateusoli.viana@gmail.com>
2019-11-14 20:53:26 -0200

    link - client server

--- server.js
+++ server.js
@@ -15,8 +15,16 @@ app.use('/', (req, res) => {
     res.render('index.html');
 });
 
+let messages = [];
+
 io.on('connection', socket => {
-    console.log('Socket conectado' + socket.id);
+    
+    socket.emit('previousMessage', messages);
+
+    socket.on('sendMessage', data => {
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
 })
 
 server.listen(3000);
