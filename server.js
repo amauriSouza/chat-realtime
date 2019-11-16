72ed1500478e7f6847de301a17dfefccd26baab1
Amauri de Souza Teixeira <amauri2015gg@gmail.com>
2019-11-15 21:06:04 -0300

    BRANCH ESPECIFICA DO PROFESSOR

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-// CONFIGURANDO MEUS PROTOCOLOS
+// CONFIGURANDO OS PROTOCOLOS
 // DEFININDO O PROTOCOLO HTTP
 const server = require('http').createServer(app);
-// DEFININDO WSS - PRO WEBSOCKET 
-// ISSO AQUI RETORNA UMA FUNÇÃO, ENTAO JA MANDO O SERVER
+// DEFININDO O PROTOCOLO WSS - WEBSOCKET 
+
+// RETORNA UMA FUNÇÃO, ENTAO JA MANDO O SERVER
 const io = require('socket.io')(server);
 
-// DEFININDO ONDE O SERVER VAI OBSERVAR MEUS ARQUIVOS FE
+// DEFININDO ONDE O SERVER VAI OBSERVAR MEUS ARQUIVOS FRONT END
 app.use(express.static(path.join(__dirname, 'public')));
 
 // DEFININDO MINHAS VIEWS
@@ -29,23 +30,12 @@ let messages = [];
 
 // ABRINDO A CONEXAO
 io.on('connection', socket => {
-    // AQUI ELE FICA ESCUTANDO OS EVENTOS QUE VEM DO FE
+    // AQUI O SOCKET.IO FICA ESCUTANDO OS EVENTOS QUE VEM DO FRONT END, COM ESSE NOME: "sendMessage"
     socket.on('sendMessage', data => {
-        // ARMAZENO AS MSGS QUE VEIO DO FE NESTE ARRAY DE MESSAGES
+        // ARMAZENO AS MSGS EM UM ARRAY DE MENSAGENS
         messages.push(data);
-        // AQUI EU EMITO A MENSAGEM ATUAL ENVIADA PELO O FE, EM TODOS OS SOQUETES CONECTADOS
-        socket.broadcast.emit('receivedMessage', data);
     });
 
-    // PEGO E ENVIO TODAS AS MENSAGENS QUE JA TEVE NA CONEXAO ATUAL DO SOCKET
-    socket.emit('previousMessage', messages);
-
-
-    // RECEBO O EVENTO DO FE DE QUEM ESTA DIGITANDO
-    socket.on('sendUserTyping', data => {
-        // AQUI EU EMITO O USUARIO ATUAL QUE ESTA DIGITANDO, EM TODOS OS SOQUETES CONECTADOS
-        socket.broadcast.emit('receivedUserTyping', data);
-    });
 });
 
 // PORTA QUE TA RODANDO
